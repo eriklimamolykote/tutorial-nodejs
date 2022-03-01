ffa7156b7b3bf9c0a3e0bc0d17baee47a9319dab
eriklimamolykote <eriklim@hotmail.com>
2022-03-01 10:59:23 -0300

    primeiro-commit-tutorial-nodejs-01-03-2022

---
+++ expressExample.js
@@ -0,0 +1,16 @@
+var express = require( 'express' );
+
+var app = express();
+
+app.get( '/', function( req, res ) {
+
+    res.send( 'Hello, World!' );
+
+});
+
+var server = app.listen( 3000, function() {
+
+
+});
+
+console.log( 'Express example running on port 3000' );
